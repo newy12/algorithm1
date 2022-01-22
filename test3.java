789ed1cf3ef4e2ae00d653cd83b433a147fd6f73
kimyoungjae <newy12@naver.com>
2022-01-23 00:04:35 +0900

    2022-01-22 test3, test4 추가

---
+++ test3.java
@@ -0,0 +1,19 @@
+public class test3 {
+    public static void main(String[] args) {
+        /*int[] absolutes = {4,7,12};
+        boolean[] signs = {true,false,true};*/
+        int[] absolutes = {1,2,3};
+        boolean[] signs = {false,false,true};
+        int sum = 0;
+
+        for (int i = 0; i < absolutes.length; i++) {
+            if(signs[i] == false) {
+                int value = absolutes[i] * -1;
+                absolutes[i] = value;
+            }
+            sum += absolutes[i];
+        }
+        System.out.println("sum = " + sum);
+    }
+
+}
---
+++ test4.java
@@ -0,0 +1,11 @@
+public class test4 {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4};
+        int[] b = {-3,-1,0,2};
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i] * b[i];
+        }
+        System.out.println("sum = " + sum);
+    }
+}
