ce06fcd302ba5e72a5779aeb47d7ed433bb229ae
kimyoungjae <newy12@naver.com>
2022-01-23 00:06:01 +0900

    2022-01-22 test3, test4 추가

--- test3.java
+++ test3.java
@@ -1,5 +1,12 @@
 public class test3 {
     public static void main(String[] args) {
+
+
+        //음양 더하기
+
+
+
+
         /*int[] absolutes = {4,7,12};
         boolean[] signs = {true,false,true};*/
         int[] absolutes = {1,2,3};
--- test4.java
+++ test4.java
@@ -1,5 +1,11 @@
 public class test4 {
     public static void main(String[] args) {
+
+
+        //내적
+
+
+
         int[] a = {1,2,3,4};
         int[] b = {-3,-1,0,2};
         int sum = 0;
