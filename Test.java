087ffa16329c48f02e8c241a969369565bc57de9
youngjae <newy12@naver.com>
2022-10-17 23:44:15 +0900

    ㅇㅇ

---
+++ Test.java
@@ -0,0 +1,28 @@
+public class Test {
+    public static void main(String[] args) {
+        class A{
+            public int x = 0;
+            public int y = 0;
+        }
+        class B extends A {
+            public int x,y;
+
+            B(int x, int y){
+                this.x = x;
+                this.y = y;
+            }
+            void print()
+            {
+                System.out.println("Base class  : {" + x + ", " + y + "}");
+                System.out.println("Base class  : {" + this.x + ", " + this.y + "}");
+                System.out.println("Super class: {" + super.x + ", " + super.y + "}");
+                //하
+            }
+        }
+
+        B obj = new B(1, 2);
+        obj.print();
+    }
+}
+
+
