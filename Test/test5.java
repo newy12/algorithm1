0f2b24a089f0c07616a5d5f0d7d48e32298f73c8
youngjae <newy12@naver.com>
2022-10-22 23:50:52 +0900

    핸드폰 번호 가리기

--- Test/test5.java
+++ Test/test5.java
@@ -12,19 +12,11 @@ public class test5{
         String phone_number2 = "027778888";
         String answer = "";
         StringBuilder stringBuilder = new StringBuilder();
-
-        System.out.println("phone_number = " + phone_number.replace(phone_number2.substring(0,phone_number2.length()-4),"*"));
         for (int i = 0; i < phone_number2.length()-4; i++) {
             stringBuilder.append(phone_number.substring(i,i+1).replaceAll("[0-9]","*"));
         };
-        stringBuilder.append(phone_number2.substring(phone_number2.length()-4));
-        System.out.println("phone_number = " + stringBuilder);
+        stringBuilder.append(phone_number.substring(phone_number.length()-4));
         answer = String.valueOf(stringBuilder);
         System.out.println("answer = " + answer);
-
-
-
-
-
     }
 }
