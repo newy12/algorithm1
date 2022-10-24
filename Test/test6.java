eda94ed97edb50a9c37457abd2811420f9b5a384
youngjae <newy12@naver.com>
2022-10-25 00:25:12 +0900

    핸드폰 번호 가리기

--- Test/test6.java
+++ Test/test6.java
@@ -10,6 +10,6 @@ public class test6 {
     public static void main(String[] args) {
         String[] answer = {"Kim","ddd"};
         String value = "김서방은 "+Arrays.asList(answer).indexOf("Kim")+"에 있다";
-        System.out.println("value = " + value);
+        System.out.println("value = " + value);//ㅇㅇ
     }
 }
