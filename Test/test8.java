1bf19343f12a7b1068244951a3bb18de0a704b0b
youngjae <newy12@naver.com>
2022-10-28 00:05:01 +0900

    해결

--- Test/test8.java
+++ Test/test8.java
@@ -5,13 +5,9 @@ package Test;
 public class test8 {
 
     public static void main(String[] args) {
-
-        String s = "abcde";
-        String a = "qwer";
-
-        System.out.println("s.length()%2 = " + s.length()%2);
-        System.out.println("s.length()%2 = " + s.length()%2);
-        System.out.println("s.length()%2 = " + s.length()%2);
+        String s = "qwer";
+        System.out.println("s.length()/22 = " + s.charAt(s.length()/2));
+        System.out.println("a.substring(1,a.length()-1) = " + s.substring(s.length()/2-1,s.length()/2+1));
     }
     
 
