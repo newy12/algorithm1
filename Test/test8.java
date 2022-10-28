71bc379af5ae24362729a6b922e3b1e22b7854a8
youngjae <newy12@naver.com>
2022-10-29 00:01:16 +0900

    해결

--- Test/test8.java
+++ Test/test8.java
@@ -5,6 +5,8 @@ package Test;
 public class test8 {
 
     public static void main(String[] args) {
+        //test
+
         String s = "qwer";
         System.out.println("s.length()/22 = " + s.charAt(s.length()/2));
         System.out.println("a.substring(1,a.length()-1) = " + s.substring(s.length()/2-1,s.length()/2+1));
