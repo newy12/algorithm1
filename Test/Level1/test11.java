9ea7e7fbbae2da07127ec21d811f31ee4af22233
youngjae <newy12@naver.com>
2022-11-01 23:32:46 +0900

    해결

--- Test/Level1/test11.java
+++ Test/Level1/test11.java
@@ -7,23 +7,17 @@ public class test11 {
     public static void main(String[] args) {
         int x = 11;
         String number = String.valueOf(x);
-        StringBuilder sb = new StringBuilder();
+        int value = 0;
 
         for (int i = 0; i < number.length(); i++) {
-           sb.append(number.charAt(i));
-
+           String  = String.valueOf(number.charAt(i));
         }
-        System.out.println("value = " + sb);
-        System.out.println("value = " + number.length());
-      /*  System.out.println("first = " + first);
-        System.out.println("second = " + second);
-        int numberFirst = Integer.parseInt(first);
-        int numberSecond = Integer.parseInt(second);
-        if(x%(numberFirst+numberSecond) == 0){
+        System.out.println("sb = " + value);
+        if(x%(Integer.parseInt(String.valueOf(value))) == 0){
             System.out.println("true");
         }else{
-            System.out.println("false");
-        }*/
+            System.out.println("false");//ㅇㅇ
+        }
     }
 
 
