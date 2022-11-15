2504a545a2495dc1854a8590128bd8101047a93c
youngjae <newy12@naver.com>
2022-11-15 23:16:08 +0900

    해결

---
+++ Test/Level1/test12.java
@@ -0,0 +1,22 @@
+package Test.Level1;
+
+import java.util.Scanner;
+
+/**
+ * 직사각형 별찍기
+ */
+public class test12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
