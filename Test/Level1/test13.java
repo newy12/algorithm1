7fcf2123eb34696664a8be44ba2c88ed0587e0d1
youngjae <newy12@naver.com>
2022-11-16 23:24:20 +0900

    해결

---
+++ Test/Level1/test13.java
@@ -0,0 +1,22 @@
+package Test.Level1;
+
+/**
+ * 자릿수 더하기
+ */
+public class test13 {
+    public static void main(String[] args) {
+        int n = 123;
+        int value = 0;
+        String num = String.valueOf(n);
+        System.out.println("num.charAt(0) = " + num.charAt(0));
+        System.out.println("num.charAt(0) = " + num.charAt(1));
+        System.out.println("num.charAt(0) = " + num.charAt(2));
+
+        for (int i = 0; i < num.length(); i++) {
+        }
+        System.out.println("value = " + value);
+    }
+
+
+
+}
