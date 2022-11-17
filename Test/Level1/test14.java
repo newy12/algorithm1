0ca584602b2ec0aad5c5e9ec0ad836edadd8d65c
youngjae <newy12@naver.com>
2022-11-17 23:45:35 +0900

    해결

---
+++ Test/Level1/test14.java
@@ -0,0 +1,18 @@
+package Test.Level1;
+
+/**
+ * 평균 구하기
+ */
+public class test14 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        double answer = 0;
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        answer = answer / arr.length;
+        System.out.println("answer = " + answer);
+    }
+
+
+}
