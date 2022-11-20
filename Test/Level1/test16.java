e06345b6bdb6b14f6df9ec0a339073b54c1e3b69
youngjae <newy12@naver.com>
2022-11-20 16:57:47 +0900

    해결

---
+++ Test/Level1/test16.java
@@ -0,0 +1,14 @@
+package Test.Level1;
+
+/**
+ * 문자열 다루기 기본
+ */
+public class test16 {
+    public static boolean main(String[] args) {
+        String s = "1234";
+        boolean answer = true;
+        String regExp = "^[0-9]{4}+$";
+        String regExp2 = "^[0-9]{6}+$";
+        return s.matches(regExp) || s.matches(regExp2);
+    }
+}
