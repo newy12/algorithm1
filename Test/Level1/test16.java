6e3f90699cccbb9e3ddd3a5775663807cee21eed
youngjae <newy12@naver.com>
2023-03-26 13:56:15 +0900

    문자열 다루기 기본 해결

--- Test/Level1/test16.java
+++ Test/Level1/test16.java
@@ -7,8 +7,8 @@ public class test16 {
     public static boolean main(String[] args) {
         String s = "1234";
         boolean answer = true;
-        String regExp = "^[0-9]{4}+$";//dd
-        String regExp2 = "^[0-9]{6}+$";//.ㅇㅇㅇㅇㅇddd
+        String regExp = "^[0-9]{4}+$";//
+        String regExp2 = "^[0-9]{6}+$";//
         return s.matches(regExp) || s.matches(regExp2);
     }
 }
